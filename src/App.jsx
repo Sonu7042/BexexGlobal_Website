ea7b508f5fc881d88370a8aa24d02bba142643c8
sonu7042 <sonusinghmourya100@gmail.com>
2025-11-11 18:27:57 +0530

    upated

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import Header from './components/Header'
 import { Outlet } from 'react-router-dom'
 import { store } from './redux/store'
 import { Provider } from 'react-redux'
+import BlueTrailGrid from './animation/BlueTrailGrid'
 
 const App = () => {
   return (
     <Provider store={store} >
+    <BlueTrailGrid/>
     <Header/>
 
-    <main className=' px-4 md:px-16 lg:px-24 py-8'>
+    <main className=' px-4 md:px-16 lg:px-24 py-8 z-50'>
     <Outlet/>
     </main>
     
